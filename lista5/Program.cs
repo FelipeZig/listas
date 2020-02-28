21ac10741b533554b310b6a4fd65821345b4413d
Felipe Zsigmond <fe_zig@hotmail.com>
2020-02-28 20:14:35 -0300

    mensagem qualquer

---
+++ lista5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lista5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
