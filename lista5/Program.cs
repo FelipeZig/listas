80275fa6b27caef1cd3ae48230a380922d786e94
Felipe Zsigmond <fe_zig@hotmail.com>
2020-02-28 21:21:31 -0300

    exercicio 1 finalizado

--- lista5/Program.cs
+++ lista5/Program.cs
@@ -8,13 +8,13 @@ namespace lista5
     {
         static void Main(string[] args)
         {
-            List<int> numberList = new List<int>(5);
+            List<int> numberList = new List<int>();
             bool sair = true;
             do
             {
                 StringBuilder menu = new StringBuilder();
-                menu.Append("\n###Escolha uma opcao:");
-                menu.Append("\n1 - Inserir os 5 numeros");
+                menu.Append("\n### Escolha uma opcao:   ###");
+                menu.Append("\n1 - Inserir os numeros");
                 menu.Append("\n2 - Imprimir numeros na ordem digitada");
                 menu.Append("\n3 - Imprimir numeros na ordem crescente");
                 menu.Append("\n4 - Imprimir numeros na ordem decrescente");
@@ -28,33 +28,42 @@ namespace lista5
                     sair = false;
                 else if (op == 1)
                 {
-                    foreach (var item in numberList)
+                    for (int i = 0; i < 5; i++)
                     {
-                        Console.WriteLine("Digite um numero:");
+                        Console.WriteLine("\nDigite um número:");
                         int num = Convert.ToInt32(Console.ReadLine());
                         numberList.Add(num);
-                    }
-                                       
+                    }                                        
                 }
                 else if (op == 2)
                 {
-                    
-                }
-                else if (op == 3)
-                {
-
-                }
-                else if (op == 4)
-                {
-
-                }
-                else if (op == 5)
-                {
-                    Console.WriteLine("\nOs itens contidos na lista sao: ");
+                    Console.WriteLine("\nOs numeros foram inseridos nesta ordem: ");
                     foreach (var item in numberList)
                     {
                         Console.WriteLine(item);
                     }
+                }
+                else if (op == 3)
+                {
+                    Console.WriteLine("\nOs numeros em ordem crescente sao: ");
+                    numberList.Sort();
+                    foreach (var item in numberList)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                else if (op == 4)
+                {
+                    Console.WriteLine("\nOs numeros em ordem decrescente sao: ");
+                    numberList.Reverse();
+                    foreach (var item in numberList)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                else if (op == 5)
+                {
+                    Console.WriteLine($"\nA lista contem {numberList.Count} elementos.");                    
                 }
                 else
                     Console.WriteLine("Opcao invalida.");
