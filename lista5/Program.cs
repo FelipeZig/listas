f7c585f16548ab0f59ff549902796703eeecd69c
Felipe Zsigmond <fe_zig@hotmail.com>
2020-03-02 15:47:39 -0300

    Entregavel

--- lista5/Program.cs
+++ lista5/Program.cs
@@ -14,69 +14,7 @@ namespace lista5
             //3 - Ordenada decrescente
             //4 - Total de itens.
 
-            //List<int> numberList = new List<int>();
-            //bool sair = true;
-            //do
-            //{
-            //    StringBuilder menu = new StringBuilder();
-            //    menu.Append("\n### Escolha uma opcao:   ###");
-            //    menu.Append("\n1 - Inserir os numeros");
-            //    menu.Append("\n2 - Imprimir numeros na ordem digitada");
-            //    menu.Append("\n3 - Imprimir numeros na ordem crescente");
-            //    menu.Append("\n4 - Imprimir numeros na ordem decrescente");
-            //    menu.Append("\n5 - Imprimir o total de itens contidos na lista");
-            //    menu.Append("\n6 - Sair");
-
-            //    Console.WriteLine(menu);
-
-            //    int op = int.Parse(Console.ReadLine());
-            //    if (op == 6)
-            //        sair = false;
-            //    else if (op == 1)
-            //    {
-            //        for (int i = 0; i < 5; i++)
-            //        {
-            //            Console.WriteLine("\nDigite um número:");
-            //            int num = Convert.ToInt32(Console.ReadLine());
-            //            numberList.Add(num);
-            //        }
-            //    }
-            //    else if (op == 2)
-            //    {
-            //        Console.WriteLine("\nOs numeros foram inseridos nesta ordem: ");
-            //        foreach (var item in numberList)
-            //        {
-            //            Console.WriteLine(item);
-            //        }
-            //    }
-            //    else if (op == 3)
-            //    {
-            //        Console.WriteLine("\nOs numeros em ordem crescente sao: ");
-            //        numberList.Sort();
-            //        foreach (var item in numberList)
-            //        {
-            //            Console.WriteLine(item);
-            //        }
-            //    }
-            //    else if (op == 4)
-            //    {
-            //        Console.WriteLine("\nOs numeros em ordem decrescente sao: ");
-            //        numberList.Reverse();
-            //        foreach (var item in numberList)
-            //        {
-            //            Console.WriteLine(item);
-            //        }
-            //    }
-            //    else if (op == 5)
-            //    {
-            //        Console.WriteLine($"\nA lista contem {numberList.Count} elementos.");
-            //    }
-            //    else
-            //        Console.WriteLine("Opcao invalida.");
-            //}
-            //while (sair);
-
-
+            Exercicio1();
 
             //Ex2 - Crie um programa que colete 5 nomes de sites e suas urls, inseridos pelo usuário, e armazene em algum tipo de lista. 
             //O nome do site e a url devem ser requisitados ao usuário em duas perguntas diferentes. 
@@ -88,114 +26,199 @@ namespace lista5
             //4 - Volte ao menu do passo 1 para nova escolha.
             //5 - Caso escolhi 2, fechar o programa.
 
-            //Dictionary<string, string> siteurl = new Dictionary<string, string>();
-
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    Console.WriteLine($"\nDigite o nome do site {i + 1}:");
-            //    string site = Console.ReadLine();
-            //    Console.WriteLine($"Digite o url do site {i + 1}:");
-            //    string url = Console.ReadLine();
-            //    siteurl.Add(site, url);
-            //}
-
-            //bool sair = true;
-            //do
-            //{
-            //    StringBuilder menu = new StringBuilder();
-            //    menu.Append("\n### Escolha uma opcao:   ###");
-            //    menu.Append("\n1 - Encontre um site");
-            //    menu.Append("\n2 - Sair");
-
-            //    Console.WriteLine(menu);
-
-            //    int op = int.Parse(Console.ReadLine());
-            //    if (op == 2)
-            //        sair = false;
-            //    else if (op == 1)
-            //    {
-            //        Console.WriteLine("\nDigite o nome do site que deseja encontrar:");
-            //        string busca = Console.ReadLine();
-            //        if (siteurl.TryGetValue(busca, out string retorno))
-            //        {
-            //            Console.WriteLine($"\nA url deste site e: {retorno}");
-            //        }
-            //        Console.WriteLine("\nO site nao existe no banco de dados");
-            //    }
-            //    else
-            //        Console.WriteLine("Opcao invalida.");
-            //}
-            //while (sair);
-
+            Exercicio2();
 
 
             //Ex.3 - Crie um programa que monte um menu com as opções 1 - Inserir número, 2 - Remover número e 3 - sair.
             //1 - Caso escolhido 1: requisite ao usuário um número e armazene.Imprima a lista sempre após a inserção.Volte para o menu.
             //2 - Caso escolhido 2, remova da lista o ***último*** número inserido.Imprima a lista após a exclusão. Volte para o menu.
             //3 - Caso escolhi 3, fechar o programa.
+
+            Exercicio3();
             
-            //Stack<int> numberStack = new Stack<int>();
-            //bool sair = true;
-            //do
-            //{
-            //    StringBuilder menu = new StringBuilder();
-            //    menu.Append("\n### Escolha uma opcao:   ###");
-            //    menu.Append("\n1 - Inserir um numero");
-            //    menu.Append("\n2 - Remover o ultimo numero inserido");                
-            //    menu.Append("\n3 - Sair");
-
-            //    Console.WriteLine(menu);
-
-            //    int op = int.Parse(Console.ReadLine());
-            //    if (op == 3)
-            //        sair = false;
-            //    else if (op == 1)
-            //    {
-            //        Console.WriteLine("\nDigite um número:");
-            //        int num = Convert.ToInt32(Console.ReadLine());
-            //        numberStack.Push(num);
-            //        Console.WriteLine("Sua lista atualmente possui o(s) seguinte(s) numero(s):");
-            //        foreach (var item in numberStack)
-            //        {
-            //            Console.WriteLine(item);
-            //        }
-            //    }
-            //    else if (op == 2)
-            //    {
-            //        if (numberStack.Count == 0)
-            //        {
-            //            Console.WriteLine("\nA lista nao possui nenhum numero para ser removido!");
-            //        }
-            //        else
-            //        {
-            //            int removido = numberStack.Pop();
-            //            Console.WriteLine($"\nO numero {removido} foi removido com sucesso!");
-            //            Console.WriteLine("\nSua lista agora possui o(s) seguinte(s) numero(s):");
-            //            if (numberStack.Count == 0)
-            //            {
-            //                Console.WriteLine("Nenhum, a lista esta vazia.");
-            //            }
-            //            else
-            //            {
-            //                foreach (var item in numberStack)
-            //                {
-            //                    Console.WriteLine(item);
-            //                }
-            //            }
-                        
-            //        }
-                    
-            //    }
-            //    else
-            //            Console.WriteLine("Opcao invalida.");
-            //}
-            //while (sair);
 
             //Ex.4 - Crie um programa que monte um menu com as opções 1 - Inserir número, 2 - Remover número e 3 - sair.
             //1 - Caso escolhido 1: requisite ao usuário um número e armazene.Imprima a lista sempre após a inserção.Volte para o menu.
             //2 - Caso escolhido 2, remova da lista o ***primeiro*** número inserido.Imprima a lista após a exclusão. Volte para o menu.
             //3 - Caso escolhi 3, fechar o programa.
 
+            Exercicio4();
+
+            
+        }
+
+        static void Exercicio1()
+        {  
+            List<int> numberList = new List<int>();
+            bool sair = true;
+            do
+            {
+                StringBuilder menu = new StringBuilder();
+                menu.Append("\n### Escolha uma opcao:   ###");
+                menu.Append("\n1 - Inserir os numeros");
+                menu.Append("\n2 - Imprimir numeros na ordem digitada");
+                menu.Append("\n3 - Imprimir numeros na ordem crescente");
+                menu.Append("\n4 - Imprimir numeros na ordem decrescente");
+                menu.Append("\n5 - Imprimir o total de itens contidos na lista");
+                menu.Append("\n6 - Sair");
+
+                Console.WriteLine(menu);
+
+                int op = int.Parse(Console.ReadLine());
+                if (op == 6)
+                    sair = false;
+                else if (op == 1)
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.WriteLine("\nDigite um número:");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        numberList.Add(num);
+                    }
+                }
+                else if (op == 2)
+                {
+                    Console.WriteLine("\nOs numeros foram inseridos nesta ordem: ");
+                    foreach (var item in numberList)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                else if (op == 3)
+                {
+                    Console.WriteLine("\nOs numeros em ordem crescente sao: ");
+                    numberList.Sort();
+                    foreach (var item in numberList)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                else if (op == 4)
+                {
+                    Console.WriteLine("\nOs numeros em ordem decrescente sao: ");
+                    numberList.Reverse();
+                    foreach (var item in numberList)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                else if (op == 5)
+                {
+                    Console.WriteLine($"\nA lista contem {numberList.Count} elementos.");
+                }
+                else
+                    Console.WriteLine("Opcao invalida.");
+            }
+            while (sair);
+        }
+
+        static void Exercicio2()
+        {
+            Dictionary<string, string> siteurl = new Dictionary<string, string>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"\nDigite o nome do site {i + 1}:");
+                string site = Console.ReadLine();
+                Console.WriteLine($"Digite o url do site {i + 1}:");
+                string url = Console.ReadLine();
+                siteurl.Add(site, url);
+            }
+
+            bool sair = true;
+            do
+            {
+                StringBuilder menu = new StringBuilder();
+                menu.Append("\n### Escolha uma opcao:   ###");
+                menu.Append("\n1 - Encontre um site");
+                menu.Append("\n2 - Sair");
+
+                Console.WriteLine(menu);
+
+                int op = int.Parse(Console.ReadLine());
+                if (op == 2)
+                    sair = false;
+                else if (op == 1)
+                {
+                    Console.WriteLine("\nDigite o nome do site que deseja encontrar:");
+                    string busca = Console.ReadLine();
+                    if (siteurl.TryGetValue(busca, out string retorno))
+                    {
+                        Console.WriteLine($"\nA url deste site e: {retorno}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nO site nao existe no banco de dados");
+                    }                    
+                }
+                else
+                    Console.WriteLine("Opcao invalida.");
+            }
+            while (sair);
+        }
+
+        static void Exercicio3()
+        {
+            Stack<int> numberStack = new Stack<int>();
+            bool sair = true;
+            do
+            {
+                StringBuilder menu = new StringBuilder();
+                menu.Append("\n### Escolha uma opcao:   ###");
+                menu.Append("\n1 - Inserir um numero");
+                menu.Append("\n2 - Remover o ultimo numero inserido");
+                menu.Append("\n3 - Sair");
+
+                Console.WriteLine(menu);
+
+                int op = int.Parse(Console.ReadLine());
+                if (op == 3)
+                    sair = false;
+                else if (op == 1)
+                {
+                    Console.WriteLine("\nDigite um número:");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    numberStack.Push(num);
+                    Console.WriteLine("Sua lista atualmente possui o(s) seguinte(s) numero(s):");
+                    foreach (var item in numberStack)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                else if (op == 2)
+                {
+                    if (numberStack.Count == 0)
+                    {
+                        Console.WriteLine("\nA lista nao possui nenhum numero para ser removido!");
+                    }
+                    else
+                    {
+                        int removido = numberStack.Pop();
+                        Console.WriteLine($"\nO numero {removido} foi removido com sucesso!");
+                        Console.WriteLine("\nSua lista agora possui o(s) seguinte(s) numero(s):");
+                        if (numberStack.Count == 0)
+                        {
+                            Console.WriteLine("Nenhum, a lista esta vazia.");
+                        }
+                        else
+                        {
+                            foreach (var item in numberStack)
+                            {
+                                Console.WriteLine(item);
+                            }
+                        }
+
+                    }
+
+                }
+                else
+                    Console.WriteLine("Opcao invalida.");
+            }
+            while (sair);
+        }
+
+        static void Exercicio4()
+        {
             Queue<int> numberQueue = new Queue<int>();
             bool sair = true;
             do
