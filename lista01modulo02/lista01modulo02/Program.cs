0f7423e16fa9a0ac9f7665932a6620f9ff2a58d3
Felipe Zsigmond <fe_zig@hotmail.com>
2020-03-09 21:29:34 -0300

    inicio

---
+++ lista01modulo02/lista01modulo02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lista01modulo02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
