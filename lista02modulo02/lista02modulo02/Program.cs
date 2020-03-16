4a38efbfe9c17ff82ce478ee51cf283cac0dc8b4
Felipe Zsigmond <fe_zig@hotmail.com>
2020-03-16 20:12:26 -0300

    teste

--- lista02modulo02/lista02modulo02/Program.cs
+++ lista02modulo02/lista02modulo02/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Text;
+using System.IO;
+
 
 namespace lista02modulo02
 {
@@ -6,7 +10,34 @@ namespace lista02modulo02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("inicio");
+            bool menusair = true;
+            do
+            {
+                StringBuilder menup = new StringBuilder();
+                menup.Append("\n###   AUTENTICACAO:   ###");
+                menup.Append("\n1 - Configurar Usuario/Senha");
+                menup.Append("\n2 - Logar");
+                menup.Append("\n3 - Limpar base");
+                menup.Append("\n4 - Sair");
+
+                Console.WriteLine(menup);
+
+                int opm = int.Parse(Console.ReadLine());
+
+                if (opm == 4)
+                    menusair = false;
+                else if (opm == 1)
+                {
+                   ConfigUsuario()
+                }
+                else if (opm == 2)
+                {
+                    Logar()
+                }
+                else
+                    Console.WriteLine("Opcao invalida, tente novamente.");
+            }
+            while (menusair);
         }
     }
 }
